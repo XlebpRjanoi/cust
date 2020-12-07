31b50dbc4b667d2fb3eca07954f2ca0459f41f24
Xlebpyshek <68370549+XlebpRjanoi@users.noreply.github.com>
2020-12-07 21:38:25 +0400

    Update instaling.sh

--- instaling.sh
+++ instaling.sh
@@ -1,16 +1,18 @@
-echo Не очкуй
 cd
-pkg install zsh &> /dev/null
-pkg install wget &> /dev/null
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)"
+pkg install -y zsh
+chsh -s /bin/zsh
+termux-reload-settings
+echo $SHELL
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd .oh-my-zsh/custom/plugins
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 git clone https://github.com/zsh-users/zsh-autosuggestions
-cd
-rm -rf .zshrc &> /dev/null
-cd cust
-mv config .zshrc &> /dev/null
-cp .zshrc $HOME &> /dev/null
-cd
-rm -rf cust &> /dev/null
-termux-reload-settings &> /dev/null
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+rm $HOME/.zshrc
+mv $HOME/cust/config $HOME/.zshrc
+termux-reload-settings
