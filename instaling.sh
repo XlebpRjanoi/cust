84dc20e1eb5a07459579692432777812e518f46e
Xlebpyshek <68370549+XlebpRjanoi@users.noreply.github.com>
2020-12-07 19:13:17 +0400

    Add files via upload

---
+++ instaling.sh
@@ -0,0 +1,12 @@
+pkg install zsh &> /dev/null
+pkg install wget &> /dev/null
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)"
+cd .oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
+cd
+rm -rf .zshrc
+cd cust
+mv config .zshrc
+cp .zshrc $HOME
+termux-reload-settings
