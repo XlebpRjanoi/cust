ba53cb40c5bbc1a39b3fcbae68fadf321dea61bc
Xlebpyshek <68370549+XlebpRjanoi@users.noreply.github.com>
2020-12-07 19:40:37 +0400

    Update instaling.sh

--- instaling.sh
+++ instaling.sh
@@ -2,10 +2,10 @@ echo Не очкуй
 cd
 pkg install zsh &> /dev/null
 pkg install wget &> /dev/null
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)" &> /dev/null
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)"
 cd .oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &> /dev/null
-git clone https://github.com/zsh-users/zsh-autosuggestions &> /dev/null
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
 cd
 rm -rf .zshrc &> /dev/null
 cd cust
