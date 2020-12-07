e2e00bb4286f56100f7a1e999515690b06d3c5bf
Xlebpyshek <68370549+XlebpRjanoi@users.noreply.github.com>
2020-12-07 19:28:15 +0400

    Update instaling.sh

--- instaling.sh
+++ instaling.sh
@@ -1,15 +1,16 @@
+print('\033[36mНе очкуй))\033[0m')
 cd
 pkg install zsh &> /dev/null
 pkg install wget &> /dev/null
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-.. -O -)" &> /dev/null
 cd .oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &> /dev/null
+git clone https://github.com/zsh-users/zsh-autosuggestions &> /dev/null
 cd
-rm -rf .zshrc
+rm -rf .zshrc &> /dev/null
 cd cust
-mv config .zshrc
-cp .zshrc $HOME
-termux-reload-settings
+mv config .zshrc &> /dev/null
+cp .zshrc $HOME &> /dev/null
 cd
-rm -rf cust
+rm -rf cust &> /dev/null
+termux-reload-settings &> /dev/null
